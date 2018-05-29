545ad141db58095e7c60f3b958bbbcb8197f0da7
starday01 <sunday@apmis.ng>
2018-05-29 10:28:23 +0100

    working on immunization appointments

--- src/services/set-multiple-appointments/set-multiple-appointments.class.js
+++ src/services/set-multiple-appointments/set-multiple-appointments.class.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+var addHours = require('date-fns/add_hours');
 class Service {
     constructor(options) {
         this.options = options || {};
@@ -15,10 +16,55 @@ class Service {
         });
     }
 
-    create(data, params) {
-        console.log(data);
+    async create(data, params) {
+        const immunizationScheduleService = this.app.service('immunization-schedule');
+        const selectedImmunizationSchedule = await immunizationScheduleService.get(data.immunizationScheduleId);
+
+        const vaccines = selectedImmunizationSchedule.vaccines;
+        const checkedVaccines = vaccines.filter(vaccine => {
+            return data.vaccineIds.find(vac => {
+                return vac == vaccine._id;
+            });
+        });
+        // console.log(selectedImmunizationSchedule);
+        // console.log(checkedVaccines);
+        // console.log(data.appointment);
+        let appointments = [];
+        let firstAppointment;
+        checkedVaccines.forEach(vaccine => {
+            vaccine.intervals.forEach((interval, i) => {
+                console.log(i);
+                if (i === 0) {
+                    if (firstAppointment == undefined) {
+                        firstAppointment = JSON.parse(JSON.stringify(data.appointment));
+                    }
+                    firstAppointment.appointmentReason = firstAppointment.appointmentReason == null ? vaccine.name + ' ' : firstAppointment.appointmentReason + ' ' + vaccine.name;
+
+                } else {
+                    const appointmentDate = addHours(new Date(), this.convertInterval(interval));
+                    console.log(appointmentDate);
+                    let currentAppointment = JSON.parse(JSON.stringify(data.appointment));
+                    currentAppointment.startDate = appointmentDate;
+                    appointments.push(currentAppointment);
+                }
+            });
+        });
+        console.log(appointments);
+        console.log(firstAppointment);
         data._id = 3493438;
         return Promise.resolve(data);
+    }
+
+    convertInterval(interval) {
+        if (interval.unit === 'Months') {
+            return interval.duration * 28 * 24;
+        } else if (interval.unit === 'Weeks') {
+            return interval.duration * 7 * 24;
+        } else if (interval.unit === 'Days') {
+            return interval.duration * 24;
+        } else if (interval.unit === 'Hours') {
+            return interval.duration;
+        }
     }
 
     update(id, data, params) {
