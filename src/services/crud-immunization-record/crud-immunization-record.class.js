e4f2ad75621b5feda9b8e31742c28bd4a32eb5e7
Chisimdi Damian Ezeanieto <chisimdi.ezeanieto@apmis.ng>
2018-07-02 12:57:20 +0100

    Done with immunization records.

--- src/services/crud-immunization-record/crud-immunization-record.class.js
+++ src/services/crud-immunization-record/crud-immunization-record.class.js
@@ -18,47 +18,53 @@ class Service {
 
     async create(data, params) {
         const immunizationRecordService = this.app.service('immunization-records');
-        // const accessToken = params.accessToken;
+        const accessToken = params.accessToken;
         const facilityId = data.facilityId;
         const immunizations = data.immunizations;
         const patientId = data.patientId;
 
         if (facilityId !== undefined) {
-            // if (accessToken !== undefined) {
-            //     const userRole = params.user.facilitiesRole.filter(x => x.facilityId.toString() === facilityId);
-            // if (userRole.length > 0) {
-            if (immunizations.length > 0) {
-                // Get patient immunization record.
-                const getPatientsImmunizationRecord = await immunizationRecordService.find({
-                    query: {
-                        facilityId: facilityId,
-                        patientId: patientId
-                    }
-                });
+            if (accessToken !== undefined) {
+                const userRole = params.user.facilitiesRole.filter(x => x.facilityId.toString() === facilityId);
+                if (userRole.length > 0) {
+                    if (immunizations.length > 0) {
+                        // Get patient immunization record.
+                        const getPatientsImmunizationRecord = await immunizationRecordService.find({
+                            query: {
+                                facilityId: facilityId,
+                                patientId: patientId
+                            }
+                        });
 
-                if (getPatientsImmunizationRecord.data.length > 0) {
-                    const immunizationRecord = getPatientsImmunizationRecord.data[0];
-                    // Merge array from the immunization record and the new array from the client
-                    immunizationRecord.immunizations = immunizationRecord.immunizations.concat(immunizations);
-                    // Update Immunization record
-                    const updateImmunizationRecord = await immunizationRecordService.patch(immunizationRecord._id, immunizationRecord, {});
-                    if (updateImmunizationRecord._id !== undefined) {
-                        return jsend.success(updateImmunizationRecord);
+                        if (getPatientsImmunizationRecord.data.length > 0) {
+                            const immunizationRecord = getPatientsImmunizationRecord.data[0];
+                            // Merge array from the immunization record and the new array from the client
+                            immunizationRecord.immunizations = immunizationRecord.immunizations.concat(immunizations);
+                            // Update Immunization record
+                            const updateImmunizationRecord = await immunizationRecordService.patch(immunizationRecord._id, immunizationRecord, {});
+                            if (updateImmunizationRecord._id !== undefined) {
+                                return jsend.success(updateImmunizationRecord);
+                            } else {
+                                return jsend.error('There was a problem updating immunization record.');
+                            }
+                        } else {
+                            // Create Immunization record with the data sent from the client
+                            const createImmunizationRecord = await immunizationRecordService.create(data);
+
+                            if (createImmunizationRecord._id !== undefined) {
+                                return jsend.success(createImmunizationRecord);
+                            } else {
+                                return jsend.error('There was a problem creating immunization record.');
+                            }
+                        }
                     } else {
-                        return jsend.error('There was a problem updating immunization record.');
+                        return jsend.error('immunizations array is empty.');
                     }
                 } else {
-                    // Create Immunization record with the data sent from the client
-                    const createImmunizationRecord = await immunizationRecordService.create(data);
-
-                    if (createImmunizationRecord._id !== undefined) {
-                        return jsend.success(createImmunizationRecord);
-                    } else {
-                        return jsend.error('There was a problem creating immunization record.');
-                    }
+                    return jsend.error('You do not have access to perform this transaction.');
                 }
             } else {
-                return jsend.error('immunizations array is empty.');
+                return jsend.error('You do not have access to perform this transaction.');
             }
         } else {
             return jsend.error('facilityId is undefined!');
