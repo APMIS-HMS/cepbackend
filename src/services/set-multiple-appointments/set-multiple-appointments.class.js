1565264c82d71e69731e176a47730872ab66674b
Sunday Orimoyegun <sunday.orimoyegun@apmis.ng>
2018-07-05 10:50:00 +0100

    update to set multiple appointment

--- src/services/set-multiple-appointments/set-multiple-appointments.class.js
+++ src/services/set-multiple-appointments/set-multiple-appointments.class.js
@@ -201,18 +201,15 @@ class Service {
     }
 
     appointmentDateBooked(appointments, comingDate) {
-        console.log(1);
         let retVal = undefined;
         for (var i = 0; i < appointments.length; i++) {
             if (isSameDay(appointments[i].startDate, comingDate)) {
                 retVal = appointments[i];
                 appointments.splice(i, 1);
-                console.log(2);
                 break;
             }
         }
 
-        console.log(3);
         return retVal;
     }
 
@@ -228,7 +225,123 @@ class Service {
         }
     }
 
-    update(id, data, params) {
+    async update(id, data, params) {
+        const immunizationRecordService = this.app.service('immunization-records');
+
+        let immunizationRecordArray = await immunizationRecordService.find({ query: { 'patientId': data.appointment.patientId } });
+        let immunizationRecordObj = immunizationRecordArray.data.length > 0 ?
+            immunizationRecordArray.data[0] :
+            undefined;
+        if (immunizationRecordObj !== undefined) {
+            let immunizations = immunizationRecordObj.immunizations;
+            immunizations =
+                immunizations.filter(e => e.appointmentId != data.appointment._id);
+
+            const immunizationScheduleService =
+                this.app.service('immunization-schedule');
+            const appointmentService = this.app.service('appointments');
+            const crudImmunizationRecordService =
+                this.app.service('crud-immunization-record');
+            const selectedImmunizationSchedule =
+                await immunizationScheduleService.get(data.immunizationScheduleId);
+
+            const vaccines = selectedImmunizationSchedule.vaccines;
+            const checkedVaccines = vaccines.filter(vaccine => {
+                return data.vaccineIds.find(vac => {
+                    return vac == vaccine._id;
+                });
+            });
+
+
+
+            let appointments = [];
+            let immunizationRecords = {
+                patientId: data.appointment.patientId,
+                facilityId: data.appointment.facilityId,
+                immunizations: []
+            };
+            let firstAppointment = data.appointment;
+            checkedVaccines.forEach(vaccine => {
+                vaccine.intervals.forEach((interval, i) => {
+                    if (i === 0) {
+                        let existingAppointment = data.appointment;
+                        existingAppointment.appointmentReason =
+                            existingAppointment.appointmentReason == null ?
+                            vaccine.name + ' ' :
+                            existingAppointment.appointmentReason + ' || ' + vaccine.name;
+
+                        let immunizationObj = {};
+                        immunizationObj.immunizationScheduleId =
+                            data.immunizationScheduleId;
+                        immunizationObj.immunizationName =
+                            selectedImmunizationSchedule.name;
+                        immunizationObj.vaccine = {
+                            name: vaccine.name,
+                            nameCode: vaccine.nameCode,
+                            code: vaccine.code,
+                            vaccinationSite: vaccine.vaccinationSite,
+                            numberOfDosage: vaccine.numberOfDosage,
+                            dosage: vaccine.dosage,
+                            serviceId: vaccine.serviceId,
+                            _id: vaccine._id
+                        }
+                        immunizationObj.sequence = interval.sequence;
+                        immunizationObj.appointmentDate = existingAppointment.startDate;
+                        immunizationObj.appointmentId = existingAppointment._id;
+                        immunizationObj.administered = false;
+
+                        immunizationRecords.immunizations.push(immunizationObj);
+
+
+                        appointments.push(existingAppointment);
+
+                    } else {
+                        const appointmentDate = addHours(
+                            data.appointment.startDate, this.convertInterval(interval));
+                        let currentAppointment =
+                            JSON.parse(JSON.stringify(data.appointment));
+
+                        currentAppointment.startDate = appointmentDate;
+                        let existingAppointment = data.appointment;
+                        existingAppointment.appointmentReason =
+                            existingAppointment.appointmentReason == null ?
+                            vaccine.name + ' ' :
+                            existingAppointment.appointmentReason + ' || ' + vaccine.name;
+
+
+                        let immunizationObj = {};
+                        immunizationObj.immunizationScheduleId =
+                            data.immunizationScheduleId;
+                        immunizationObj.immunizationName =
+                            selectedImmunizationSchedule.name;
+                        immunizationObj.vaccine = {
+                            name: vaccine.name,
+                            nameCode: vaccine.nameCode,
+                            code: vaccine.code,
+                            vaccinationSite: vaccine.vaccinationSite,
+                            numberOfDosage: vaccine.numberOfDosage,
+                            dosage: vaccine.dosage,
+                            serviceId: vaccine.serviceId,
+                            _id: vaccine._id
+                        }
+                        immunizationObj.sequence = interval.sequence;
+                        immunizationObj.appointmentDate = existingAppointment.startDate;
+                        immunizationObj.appointmentId = existingAppointment._id;
+                        immunizationObj.administered = false;
+
+                        immunizationRecords.immunizations.push(immunizationObj);
+
+
+                        appointments.push(existingAppointment);
+                    }
+                });
+            });
+
+            return Promise.resolve({ appointments });
+        }
+
+
+
         return Promise.resolve(data);
     }
 
