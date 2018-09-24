7bac2535fdef8848576633c686fc7bc67a1e7167
Liztank <lizzyladi@hotmail.com>
2018-09-24 18:01:20 +0100

    validation added to inventory models

--- src/models/inventories.model.js
+++ src/models/inventories.model.js
@@ -9,15 +9,15 @@ module.exports = function (app) {
     const mongooseClient = app.get('mongooseClient');
     const { Schema } = mongooseClient;
     const inventories = new Schema({
-        facilityId: { type: Schema.Types.ObjectId, require: true, index: true },
-        storeId: { type: Schema.Types.ObjectId, require: true, index: true },
+        facilityId: { type: Schema.Types.ObjectId, require: [true,'Facility Id not supplied or Invalid'], index: true },
+        storeId: { type: Schema.Types.ObjectId, require: [true, 'Invalid store Id'], index: true },
         serviceId: { type: Schema.Types.ObjectId, require: false },
         categoryId: { type: Schema.Types.ObjectId, require: false },
         facilityServiceId: { type: Schema.Types.ObjectId, require: false },
-        productId: { type: Schema.Types.ObjectId, require: true, index: true },
+        productId: { type: Schema.Types.ObjectId, require: [true,'Invalid product Id'], index: true },
         productObject: { type: Schema.Types.Mixed, require: true, index: true },
-        totalQuantity: { type: Number, required: true },
-        availableQuantity:{ type: Number, required: true },
+        totalQuantity: { type: Number, required: [true,'Total quantity (Number) field is required.'] },
+        availableQuantity:{ type: Number, required: [true,'Available quntity (Number) field must not be empty.'] },
         reorderLevel: { type: Number, required: false },
         reorderQty: { type: Number, required: false },
         transactions: [inventoryTransactionSchema]
--- src/services/inventory-initialisers/inventory-initialisers.class.js
+++ src/services/inventory-initialisers/inventory-initialisers.class.js
@@ -1,107 +1,107 @@
 /* eslint-disable no-unused-vars */
 class Service {
-  constructor(options) {
-    this.options = options || {};
-  }
-
-  setup(app) {
-    this.app = app;
-  }
-
-  find(params) {
-    return Promise.resolve([]);
-  }
-
-  get(id, params) {
-    return Promise.resolve({
-      id,
-      text: `A new message with ID: ${id}!`
-    });
-  }
-
-  async create(data, params) {
-    console.log(data);
-    const inventoriesService = this.app.service('inventories');
-    const productsService = this.app.service('products');
-    const orgService = this.app.service('organisation-services');
-    let inventory = await inventoriesService.find({
-      query: {
-        facilityId: data.product.facilityId,
-        productId: data.product.productObject.id,
-        storeId:data.storeId
-      }
-    });
-    if (inventory.data.length > 0) {
-      return {};
-    } else {
-      let service = {};
-      let index = null;
-      let orgServiceValue = {};
-      service.name = data.product.productObject.name;
-      let awaitOrganService = await orgService.get(data.facilityServiceId);
-      awaitOrganService.categories.forEach((item, i) => {
-        if (item._id.toString() === data.categoryId.toString()) {
-          item.services.push(service);
-          index = i;
-        }
-      });
-      const payResult = await orgService.patch(awaitOrganService._id, awaitOrganService);
-      payResult.categories.forEach((itemi, i) => {
-        if (itemi._id.toString() === data.categoryId.toString()) {
-          itemi.services.forEach((items, s) => {
-            if (items.name === service.name) {
-              orgServiceValue.serviceId = items._id;
-            }
-          });
-        }
-      });
-
-      let batches = data;
-      let inventoryModel = {};
-      inventoryModel.facilityId = batches.product.facilityId;
-      inventoryModel.storeId = batches.storeId;
-      inventoryModel.serviceId = orgServiceValue.serviceId;
-      inventoryModel.categoryId = data.categoryId;
-      inventoryModel.facilityServiceId = data.facilityServiceId;
-      inventoryModel.productId = batches.product.productObject.id;
-      inventoryModel.productObject = batches.product.productObject;
-      inventoryModel.transactions = [];
-      inventoryModel.totalQuantity = 0;
-      inventoryModel.availableQuantity = 0;
-
-      let len = batches.batchItems.length - 1;
-      for (let index = len; index >= 0; index--) {
-        if (index >= 0) {
-          inventoryModel.totalQuantity += batches.batchItems[index].quantity;
-          inventoryModel.availableQuantity += batches.batchItems[index].quantity;
-          inventoryModel.transactions.push(batches.batchItems[index]);
-        }
-      }
-      let inventory = await inventoriesService.create(inventoryModel);
-      let res = {
-        inventory: inventory
-      };
-      return res;
+    constructor(options) {
+        this.options = options || {};
     }
-  }
 
-  update(id, data, params) {
-    return Promise.resolve(data);
-  }
+    setup(app) {
+        this.app = app;
+    }
 
-  patch(id, data, params) {
-    return Promise.resolve(data);
-  }
+    find(params) {
+        return Promise.resolve([]);
+    }
 
-  remove(id, params) {
-    return Promise.resolve({
-      id
-    });
-  }
+    get(id, params) {
+        return Promise.resolve({
+            id,
+            text: `A new message with ID: ${id}!`
+        });
+    }
+
+    async create(data, params) {
+        console.log(data);
+        const inventoriesService = this.app.service('inventories');
+        const productsService = this.app.service('products');
+        const orgService = this.app.service('organisation-services');
+        let inventory = await inventoriesService.find({
+            query: {
+                facilityId: data.product.facilityId,
+                productId: data.product.productObject.id,
+                storeId:data.storeId
+            }
+        });
+        if (inventory.data.length > 0) {
+            return {};
+        } else {
+            let service = {};
+            let index = null;
+            let orgServiceValue = {};
+            service.name = data.product.productObject.name;
+            let awaitOrganService = await orgService.get(data.facilityServiceId);
+            awaitOrganService.categories.forEach((item, i) => {
+                if (item._id.toString() === data.categoryId.toString()) {
+                    item.services.push(service);
+                    index = i;
+                }
+            });
+            const payResult = await orgService.patch(awaitOrganService._id, awaitOrganService);
+            payResult.categories.forEach((itemi, i) => {
+                if (itemi._id.toString() === data.categoryId.toString()) {
+                    itemi.services.forEach((items, s) => {
+                        if (items.name === service.name) {
+                            orgServiceValue.serviceId = items._id;
+                        }
+                    });
+                }
+            });
+
+            let batches = data;
+            let inventoryModel = {};
+            inventoryModel.facilityId = batches.product.facilityId;
+            inventoryModel.storeId = batches.storeId;
+            inventoryModel.serviceId = orgServiceValue.serviceId;
+            inventoryModel.categoryId = data.categoryId;
+            inventoryModel.facilityServiceId = data.facilityServiceId;
+            inventoryModel.productId = batches.product.productObject.id;
+            inventoryModel.productObject = batches.product.productObject;
+            inventoryModel.transactions = [];
+            inventoryModel.totalQuantity = 0;
+            inventoryModel.availableQuantity = 0;
+
+            let len = batches.batchItems.length - 1;
+            for (let index = len; index >= 0; index--) {
+                if (index >= 0) {
+                    inventoryModel.totalQuantity += batches.batchItems[index].quantity;
+                    inventoryModel.availableQuantity += batches.batchItems[index].quantity;
+                    inventoryModel.transactions.push(batches.batchItems[index]);
+                }
+            }
+            let inventory = await inventoriesService.create(inventoryModel);
+            let res = {
+                inventory: inventory
+            };
+            return res;
+        }
+    }
+
+    update(id, data, params) {
+        return Promise.resolve(data);
+    }
+
+    patch(id, data, params) {
+        return Promise.resolve(data);
+    }
+
+    remove(id, params) {
+        return Promise.resolve({
+            id
+        });
+    }
 }
 
 module.exports = function (options) {
-  return new Service(options);
+    return new Service(options);
 };
 
 module.exports.Service = Service;
