f15a432266223195d575a99e0d898e8b1fe24264
Sunday Orimoyegun <sunday.orimoyegun@apmis.ng>
2018-10-09 12:33:26 +0100

    updates

--- src/services/health-covered-bill-histories/health-covered-bill-histories.class.js
+++ src/services/health-covered-bill-histories/health-covered-bill-histories.class.js
@@ -17,75 +17,65 @@ class Service {
 
     const startDate = format(params.query.startDate);
     const bills = await billingService.find({
-      query: {
-        isCoveredPage: true,
-        facilityId: params.query.facilityId,
-        $or: [{
-          'billItems.covered.hmoId': params.query.hmoId
-        }, {
-          'billItems.covered.familyId': params.query.hmoId
-        },{
-          'billItems.covered.companyId': params.query.hmoId
-        }],
-        $and: [{
-            updatedAt: {
-              $gte: startDate
-            }
-          },
-          {
-            updatedAt: {
-              $lte: endDate
-            }
-          }
+        query: {
+          isCoveredPage: true,
+          facilityId: params.query.facilityId,
+          $or: [{
+            'billItems.covered.hmoId': params.query.hmoId
+          }, {
+            'billItems.covered.familyId': params.query.hmoId
+          }, {
+            'billItems.covered.companyId': params.query.hmoId
+          }]
         ],
-        $select:['coverFile','billItems.covered','billItems.patientId','billItems.facilityServiceId','billItems.serviceId']
+        $select: ['coverFile', 'billItems.covered', 'billItems.patientId', 'billItems.facilityServiceId', 'billItems.serviceId']
       }
     });
-    
-    bills.data.map(element => {
-      const index = element.billItems.map(x => x.covered.isVerify !== undefined);
-      if (index.length === 0) {
-        element.isPending = true;
-      } else {
-        element.isPending = false;
-      }
-    });
-    const pendingBills = bills.data.filter(x => x.isPending === true);
-    const historyBills = bills.data.filter(x => x.isPending === false);
-    return jsend.success({
-      pendingBills: pendingBills,
-      historyBills: historyBills
-    });
-  }
 
-  get(id, params) {
-    return Promise.resolve({
-      id,
-      text: `A new message with ID: ${id}!`
-    });
-  }
-
-  create(data, params) {
-    if (Array.isArray(data)) {
-      return Promise.all(data.map(current => this.create(current, params)));
+  bills.data.map(element => {
+    const index = element.billItems.map(x => x.covered.isVerify !== undefined);
+    if (index.length === 0) {
+      element.isPending = true;
+    } else {
+      element.isPending = false;
     }
+  });
+  const pendingBills = bills.data.filter(x => x.isPending === true);
+  const historyBills = bills.data.filter(x => x.isPending === false);
+  return jsend.success({
+    pendingBills: pendingBills,
+    historyBills: historyBills
+  });
+}
 
-    return Promise.resolve(data);
+get(id, params) {
+  return Promise.resolve({
+    id,
+    text: `A new message with ID: ${id}!`
+  });
+}
+
+create(data, params) {
+  if (Array.isArray(data)) {
+    return Promise.all(data.map(current => this.create(current, params)));
   }
 
-  update(id, data, params) {
-    return Promise.resolve(data);
-  }
+  return Promise.resolve(data);
+}
 
-  patch(id, data, params) {
-    return Promise.resolve(data);
-  }
+update(id, data, params) {
+  return Promise.resolve(data);
+}
 
-  remove(id, params) {
-    return Promise.resolve({
-      id
-    });
-  }
+patch(id, data, params) {
+  return Promise.resolve(data);
+}
+
+remove(id, params) {
+  return Promise.resolve({
+    id
+  });
+}
 }
 
 module.exports = function (options) {
