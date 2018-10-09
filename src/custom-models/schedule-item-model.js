50e992ff7f6250ef65232dadf85662e4e717a0f8
Sunday Orimoyegun <sunday.orimoyegun@apmis.ng>
2018-10-09 10:32:33 +0100

    update to schdule-item-model

--- src/custom-models/schedule-item-model.js
+++ src/custom-models/schedule-item-model.js
@@ -2,11 +2,41 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const scheduleItemSchema = new Schema({
-  day: { type: String, required: true },
-  startTime: { type: Date, required: true },
-  endTime: { type: Date, required: true },
-  location: { type: Schema.Types.Mixed, required: true },
-  createdAt: { type: Date, 'default': Date.now },
-  updatedAt: { type: Date, 'default': Date.now }
+  day: {
+    type: String,
+    required: true
+  },
+  startTime: {
+    type: Date,
+    required: true
+  },
+  endTime: {
+    type: Date,
+    required: true
+  },
+  location: {
+    type: Schema.Types.Mixed,
+    required: true
+  },
+  noSlots: {
+    type: Schema.Types.Number,
+    required: false
+  },
+  timePerSlot: {
+    type: Schema.Types.Number,
+    required: false
+  },
+  timeUnit: {
+    type: String,
+    required: false
+  },
+  createdAt: {
+    type: Date,
+    'default': Date.now
+  },
+  updatedAt: {
+    type: Date,
+    'default': Date.now
+  }
 });
 module.exports = scheduleItemSchema;
