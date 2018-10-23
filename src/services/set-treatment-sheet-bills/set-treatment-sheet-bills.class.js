b803e56d55a6a0fb1a0826985bf146aec2a813d9
farquardsolve <fakuadesegun@live.com>
2018-10-23 23:23:06 +0100

    update

--- src/services/set-treatment-sheet-bills/set-treatment-sheet-bills.class.js
+++ src/services/set-treatment-sheet-bills/set-treatment-sheet-bills.class.js
@@ -23,7 +23,7 @@ class Service {
     const billCreatorsService = this.app.service('bill-creators');
     const patientsService = this.app.service('patients');
 
-    
+
     const patient = await patientsService.find({
       query: {
         facilityId: data.facilityId,
@@ -31,21 +31,25 @@ class Service {
         $select: ['_id', 'clientsNo', 'paymentPlan']
       }
     });
-    
+
     let bills = [];
     let procedureBills = [];
     let medicationBills = [];
     let investigationItems = [];
-    let billCreator ={};
-    
+    let billCreator = {};
+    let labRequests = {};
+
     let billCreatorPrecedure = {};
     let billCreatorMed = {};
     
+    const treatmentSheet = await treatmentSheetsService.create(data);
+
+
     if (data.treatmentSheet.investigations !== undefined) {
-      
-      data.treatmentSheet.investigations.map(investigation => {
+      data.treatmentSheet.investigations.forEach(investigation => {
+        let bill = {};
         if (investigation.isBilled === true) {
-          const bill = {
+          bill = {
             unitPrice: 0,
             facilityId: data.facilityId,
             unitPriceChanges: [],
@@ -59,26 +63,8 @@ class Service {
               coverType: patient.data[0].paymentPlan.find(x => x.isDefault !== true).planType
             },
           };
-
-          const item = {
-            isUrgent: false,
-            isExternal: false,
-            investigation: {
-              isPanel: false,
-              LaboratoryWorkbenches: investigation.LaboratoryWorkbenches,
-              facilityServiceId: investigation.facilityServiceId,
-              serviceId: investigation.serviceId,
-              reportType: investigation.reportType,
-              specimen: investigation.specimen,
-              unit: investigation.unit,
-              name: investigation.name,
-              facilityId: data.facilityId
-            }
-          }
-          investigationItems.push(item);
-
           if (investigation.investigation !== undefined) {
-            bill.unitPrice = (investigation.investigation.changedPrice === undefined) ? investigation.investigation.location.workbenches[0].price : investigation.investigation.changedPrice;
+            bill.unitPrice = (investigation.investigation.changedPrice !== null && investigation.investigation.changedPrice !== undefined) ? investigation.investigation.changedPrice : investigation.investigation.location.workbenches[0].price;
             bill.totalPrice = bill.unitPrice;
             if (investigation.investigation.changedPrice !== undefined) {
               bill.unitPriceChanges.push({
@@ -91,37 +77,46 @@ class Service {
 
           bills.push(bill);
         }
+        const item = {
+          isUrgent: false,
+          isExternal: (investigation.isBilled === true) ? false : true,
+          investigation: {
+            isPanel: false,
+            LaboratoryWorkbenches: investigation.LaboratoryWorkbenches,
+            facilityServiceId: investigation.facilityServiceId,
+            serviceId: investigation.serviceId,
+            reportType: investigation.reportType,
+            specimen: investigation.specimen,
+            unit: investigation.unit,
+            name: investigation.name,
+            facilityId: data.facilityId
+          }
+        }
+        investigationItems.push(item);
       });
-      if(bills.length>0){
-    
+      if (bills.length > 0) {
         billCreator = await billCreatorsService.create(bills, {
           query: {
             facilityId: data.facilityId,
             patientId: patient.data[0]._id
           }
         });
-    
-        const laboratoryRequests = {
-          facilityId: data.facilityId,
-          patientId: patient.data[0]._id,
-          createdBy: params.user._id,
-          clinicalInformation: "N/A",
-          diagnosis: "N/A",
-          investigations: investigationItems,
-          billingId: billCreator
-        };
-    
-        labrequestService.create(laboratoryRequests).then(payload=>{
-          _requests = payload;
-        });
-        
       }
+      const laboratoryRequests = {
+        facilityId: data.facilityId,
+        patientId: patient.data[0]._id,
+        createdBy: params.user._id,
+        clinicalInformation: "N/A",
+        diagnosis: "N/A",
+        investigations: investigationItems,
+        billingId: billCreator
+      };
+      labrequestService.create(laboratoryRequests);
     }
-    
     if (data.treatmentSheet.procedures !== undefined) {
-      data.treatmentSheet.procedures.map(procedure => {
+      data.treatmentSheet.procedures.forEach(procedure => {
         const bill = {
-          unitPrice: (procedure.changedPrice === undefined || procedure.changedPrice === null) ? procedure.price.price : procedure.changedPrice,
+          unitPrice: (procedure.changedPrice !== undefined && procedure.changedPrice !== null) ? procedure.changedPrice : procedure.price.price,
           facilityId: data.facilityId,
           unitPriceChanges: [],
           facilityServiceId: procedure.price.facilityServiceId,
@@ -129,7 +124,7 @@ class Service {
           patientId: patient.data[0]._id,
           quantity: 1,
           active: true,
-          totalPrice: (procedure.changedPrice === undefined || procedure.changedPrice === null) ? procedure.price.price : procedure.changedPrice,
+          totalPrice: (procedure.changedPrice !== undefined && procedure.changedPrice !== null) ? procedure.changedPrice : procedure.price.price,
           covered: {
             coverType: patient.data[0].paymentPlan.find(x => x.isDefault !== true).planType
           },
@@ -143,51 +138,50 @@ class Service {
           patientId: patient.data[0]._id
         }
       });
-    
     }
-    
     if (data.treatmentSheet.medications !== undefined) {
-      data.treatmentSheet.medications.map(medication => {
-        const bill = {
-          unitPrice: (medication.changedPrice === undefined || medication.changedPrice === null) ? medication.cost : medication.changedPrice,
-          facilityId: data.facilityId,
-          unitPriceChanges: [],
-          facilityServiceId: medication.facilityServiceId,
-          serviceId: medication.serviceId,
-          patientId: patient.data[0]._id,
-          quantity: medication.quantity,
-          active: true,
-          totalPrice: medication.quantity * ((medication.changedPrice === undefined || medication.changedPrice === null) ? medication.cost : medication.changedPrice),
-          covered: {
-            coverType: patient.data[0].paymentPlan.find(x => x.isDefault !== true).planType
-          },
-        };
-        if (medication.changedPrice !== undefined) {
-          bill.unitPriceChanges.push({
-            currentPrice: medication.changedPrice,
-            oldPrice: medication.cost,
-            userId: params.user._id
-          });
-        }
-        medicationBills.push(bill);
-      });
-      billCreatorMed = await billCreatorsService.create(medicationBills, {
-        query: {
-          facilityId: data.facilityId,
-          patientId: patient.data[0]._id
+      data.treatmentSheet.medications.forEach(medication => {
+        if (medication.isBilled) {
+          const bill = {
+            unitPrice: (medication.changedPrice !== undefined && medication.changedPrice !== null) ? medication.changedPrice : medication.cost,
+            facilityId: data.facilityId,
+            unitPriceChanges: [],
+            facilityServiceId: medication.facilityServiceId,
+            serviceId: medication.serviceId,
+            patientId: patient.data[0]._id,
+            quantity: medication.quantity,
+            active: true,
+            totalPrice: medication.quantity * ((medication.changedPrice !== undefined && medication.changedPrice !== null) ? medication.changedPrice : medication.cost),
+            covered: {
+              coverType: patient.data[0].paymentPlan.find(x => x.isDefault !== true).planType
+            },
+          };
+          if (medication.changedPrice !== undefined && medication.changedPrice !== null) {
+            bill.unitPriceChanges.push({
+              currentPrice: medication.changedPrice,
+              oldPrice: medication.cost,
+              userId: params.user._id
+            });
+          }
+          medicationBills.push(bill);
         }
       });
+      if (medicationBills.length > 0) {
+        billCreatorMed = await billCreatorsService.create(medicationBills, {
+          query: {
+            facilityId: data.facilityId,
+            patientId: patient.data[0]._id
+          }
+        });
+      }
     }
-    
-    const treatmentSheet = await treatmentSheetsService.create(data);
-    
     const result = {
       investigations: billCreator,
       procedures: billCreatorPrecedure,
       medications: billCreatorMed,
       treatmentSheet: treatmentSheet
     }
-    
+
     return jsend.success(result);
   }
 
