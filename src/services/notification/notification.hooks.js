743e71a32bcea97c9f72d75089cf65db1e42bcc3
Liztank <lizzyladi@hotmail.com>
2018-10-30 11:12:11 +0100

    notification update

--- src/services/notification/notification.hooks.js
+++ src/services/notification/notification.hooks.js
@@ -1,33 +1,50 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
+const {
+    fastJoin
+} = require('feathers-hooks-common');
+
+const resolvers = {
+    joins: {
+        loggedInUser: () => async (notification, context) => {
+            console.log('========hook context=========\n',context);
+            notification.senderId = context.user._id ;
+            notification.isRead = false ;
+
+        }
+    }
+
+};
+
+
 
 module.exports = {
-  before: {
-    all: [ authenticate('jwt') ],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  },
+    before: {
+        all: [authenticate('jwt')],
+        find: [],
+        get: [],
+        create: [fastJoin(resolvers)],
+        update: [],
+        patch: [],
+        remove: []
+    },
 
-  after: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  },
+    after: {
+        all: [],
+        find: [],
+        get: [],
+        create: [],
+        update: [],
+        patch: [],
+        remove: []
+    },
 
-  error: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  }
+    error: {
+        all: [],
+        find: [],
+        get: [],
+        create: [],
+        update: [],
+        patch: [],
+        remove: []
+    }
 };
