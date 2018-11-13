10035a5045cdae28ea2638248d2d7e9abc3983a6
Sunday Orimoyegun <sunday.orimoyegun@apmis.ng>
2018-11-13 18:31:03 +0100

    updates

--- src/services/laboratory-report-summary/laboratory-report-summary.class.js
+++ src/services/laboratory-report-summary/laboratory-report-summary.class.js
@@ -1,63 +1,77 @@
 /* eslint-disable no-unused-vars */
 const jsend = require('jsend');
 class Service {
-    constructor(options) {
-        this.options = options || {};
-    }
+  constructor(options) {
+    this.options = options || {};
+  }
 
-    find(params) {
-        return Promise.resolve([]);
-    }
+  find(params) {
+    return Promise.resolve([]);
+  }
 
-    get(id, params) {
-        return Promise.resolve({
-            id, text: `A new message with ID: ${id}!`
-        });
-    }
+  get(id, params) {
+    return Promise.resolve({
+      id,
+      text: `A new message with ID: ${id}!`
+    });
+  }
 
-    async create(data, params) {
-        const InvestigationService = this.app.service('investigations');
-        const LaboratoryRequestService = this.app.service('laboratory-requests');
 
-        let facilityId = data.facilityId;
-        try {
-            console.log('========Got here=========\n',facilityId);
-            const getLabRequest = await LaboratoryRequestService.find({query:{facilityId:facilityId}});
-             console.log('========Got here1=========');
-            if(getLabRequest !== undefined){
-                console.log('==============data=============\n',getLabRequest);
-            }
-        } catch (error) {
-            console.log('===error===\n',error);
-            return jsend.error({message:'There was an errror while ',code:422, data:{error} });
+  async create(data, params) {
+    const InvestigationService = this.app.service('investigations');
+    const LaboratoryRequestService = this.app.service('laboratory-requests');
+
+    let facilityId = data.facilityId;
+    try {
+      console.log('========Got here=========\n', facilityId);
+      const getLabRequest = await LaboratoryRequestService.find({
+        query: {
+          facilityId: facilityId
         }
-
-        if (Array.isArray(data)) {
-            return Promise.all(data.map(current => this.create(current, params)));
+      });
+      console.log('========Got here1=========');
+      if (getLabRequest !== undefined) {
+        console.log('==============data=============\n', getLabRequest);
+      }
+    } catch (error) {
+      console.log('===error===\n', error);
+      return jsend.error({
+        message: 'There was an errror while ',
+        code: 422,
+        data: {
+          error
         }
-
-        return Promise.resolve(data);
+      });
     }
 
-    update(id, data, params) {
-        return Promise.resolve(data);
+    if (Array.isArray(data)) {
+      return Promise.all(data.map(current => this.create(current, params)));
     }
 
-    patch(id, data, params) {
-        return Promise.resolve(data);
-    }
+    return Promise.resolve(data);
+  }
 
-    remove(id, params) {
-        return Promise.resolve({ id });
-    }
+  update(id, data, params) {
+    return Promise.resolve(data);
+  }
 
-    setup(app) {
-        this.app = app;
-    }
+  patch(id, data, params) {
+    return Promise.resolve(data);
+  }
+
+  remove(id, params) {
+    return Promise.resolve({
+      id
+    });
+  }
+
+  setup(app) {
+    this.app = app;
+  }
 }
 
 module.exports = function (options) {
-    return new Service(options);
+  return new Service(options);
 };
 
 module.exports.Service = Service;
--- src/services/laboratory-requests/laboratory-requests.hooks.js
+++ src/services/laboratory-requests/laboratory-requests.hooks.js
@@ -1,47 +1,61 @@
-const { authenticate } = require('@feathersjs/authentication').hooks;
-const { fastJoin } = require('feathers-hooks-common');
+const {
+  authenticate
+} = require('@feathersjs/authentication').hooks;
+const {
+  fastJoin
+} = require('feathers-hooks-common');
 const resolvers = {
-    joins: {
-        patientDetails: () => async(request, context) => {
-            const patient = await context.app.service('patients').get(request.patientId, {});
-            request.personDetails = patient.personDetails;
-        },
-        employeeDetails: () => async(request, context) => {
-            const employee = await context.app.service('employees').get(request.createdBy, {});
-            request.employeeDetails = employee.personDetails;
-        }
+  joins: {
+
+    patientDetails: () => async (request, context) => {
+      try {
+        const patient = await context.app.service('patients').get(request.patientId, {});
+        request.personDetails = patient.personDetails;
+      } catch (error) {
+        console.log('error1: ', error);
+      }
+
+    },
+    employeeDetails: () => async (request, context) => {
+      try {
+        console.log(request.createdBy);
+        const employee = await context.app.service('employees').get(request.createdBy, {});
+        request.employeeDetails = employee.personDetails;
+      } catch (error) {}
+
     }
+  }
 };
 
 
 module.exports = {
-    before: {
-        all: [authenticate('jwt')],
-        find: [],
-        get: [],
-        create: [],
-        update: [],
-        patch: [],
-        remove: []
-    },
+  before: {
+    all: [authenticate('jwt')],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
 
-    after: {
-        all: [fastJoin(resolvers)],
-        find: [],
-        get: [],
-        create: [],
-        update: [],
-        patch: [],
-        remove: []
-    },
+  after: {
+    all: [fastJoin(resolvers)],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
 
-    error: {
-        all: [],
-        find: [],
-        get: [],
-        create: [],
-        update: [],
-        patch: [],
-        remove: []
-    }
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  }
 };
