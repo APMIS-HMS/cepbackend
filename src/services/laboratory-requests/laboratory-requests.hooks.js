2e490c473f0bd039bb4dc3a83dc9099ed86d6705
Sunday Orimoyegun <sunday.orimoyegun@apmis.ng>
2018-11-14 10:04:14 +0100

    updates to remodelling laboratory report

--- src/services/laboratory-requests/laboratory-requests.hooks.js
+++ src/services/laboratory-requests/laboratory-requests.hooks.js
@@ -11,14 +11,11 @@ const resolvers = {
       try {
         const patient = await context.app.service('patients').get(request.patientId, {});
         request.personDetails = patient.personDetails;
-      } catch (error) {
-        console.log('error1: ', error);
-      }
+      } catch (error) {}
 
     },
     employeeDetails: () => async (request, context) => {
       try {
-        console.log(request.createdBy);
         const employee = await context.app.service('employees').get(request.createdBy, {});
         request.employeeDetails = employee.personDetails;
       } catch (error) {}
