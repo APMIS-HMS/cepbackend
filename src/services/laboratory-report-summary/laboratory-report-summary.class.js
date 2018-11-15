92a9136445a74bab8e05f64c180c2f8980208ee4
Liztank <lizzyladi@hotmail.com>
2018-11-15 18:20:42 +0100

    completed diagnosis and added appoitment Summary service

--- src/services/laboratory-report-summary/laboratory-report-summary.class.js
+++ src/services/laboratory-report-summary/laboratory-report-summary.class.js
@@ -5,7 +5,159 @@ class Service {
         this.options = options || {};
     }
 
-    find(params) {
+    async find(params) {
+        const LabRequestService = this.app.service('laboratory-requests');
+        const LabReportService = this.app.service('laboratory-reports');
+        const FacilityService = this.app.service('facilities');
+
+        let facilityId = params.query.facilityId;
+        console.log(facilityId);
+        let labReportSummary ={
+            apmisId : String,
+            patientName : String,
+            status : String,
+            doctor :String,
+            request : Number,
+            date  : Date
+        };
+        
+        let summaryByLoc ={
+            request :String,
+            total  : Number 
+        };
+        let summaryByBench={
+            name:String,
+            total:Number
+        };
+        let labReportByLoc ={
+            location:String,
+            summary:summaryByLoc
+        };
+
+        let labReportByBench = {
+            bench:String,
+            summary:summaryByBench
+        };
+        try { 
+            let getLabReport;
+            if(params.query.startDate !== undefined && params.query.endDate===undefined){
+                //Get all reports filtered by facilityId and date range
+                getLabReport = await LabReportService.find({
+                    query: {
+                        facilityId: facilityId,
+                        $and: [{
+                            updatedAt: {
+                                $gte: params.query.startDate
+                            }
+                        },
+                        {
+                            updatedAt: {
+                                $lte: Date.now()
+                            }
+                        }
+                        ] 
+                    }
+                });
+            }
+            else if(params.query.startDate !==undefined && params.query.endDate!==undefined){
+                //Get all reports filtered by facilityId and date range
+                getLabReport = await LabReportService.find({
+                    query: {
+                        facilityId: facilityId,
+                        $and: [{
+                            updatedAt: {
+                                $gte: params.query.startDate
+                            }
+                        },
+                        {
+                            updatedAt: {
+                                $lte: params.query.endDate
+                            }
+                        }
+                        ] 
+                    }
+                });
+            }// Search by querystring
+            else if(params.query.queryString !== undefined){
+                getLabReport = await LabReportService.find({query:query.params});
+            }
+            // Search by all
+            else if(params.query.searchBy !== undefined){
+                getLabReport = await LabReportService.find({query:{facilityId: facilityId}});
+            }
+            else{
+                getLabReport = await LabReportService.find({
+                    query: {
+                        facilityId: facilityId,
+                        updatedAt: { $lte: Date.now() }
+
+                    }
+                });
+            }
+            
+           
+            //Filter request Ids from report
+            let requestIds = getLabReport.data.map(x=>{
+                return x.requestId;
+            });
+            //Get requests by Ids
+            let labRequests = await LabRequestService.find({query:{
+                '_id':{$in:requestIds},
+            }});
+
+            let workbenches = getLabReport.data.map(x=>{
+                return x.workBench;
+            });
+            let locations = workbenches.map(x=>{
+                return x.minorLocationId;
+            });
+            
+            let getFac = await FacilityService.find({query:{facilityId:facilityId}});
+
+            if(params.query.location !==true){
+                summaryByLoc.total = locations.length;
+            }
+            if(params.query.bench !== undefined){
+                summaryByBench.total = workbenches.length;
+            }
+            let summary=[];
+            let laboratoryReport={};
+            let newLapRequest=labRequests.data.map(x=>{
+                return {
+                    patientDetails:x.personDetails,
+                    investigation:x.investigations,
+                    patientId:x.patientId,
+                    status:x.isUploaded,
+                    date:x.createdAt
+
+                };
+            });
+            if (labRequests.data.length >0) {
+                newLapRequest.forEach((patient,i) =>{
+                    labReportSummary.apmisId = patient.patientDetails.apmisId;
+                    labReportSummary.patientName = patient.patientDetails.firstName;
+                    labReportSummary.patientId = patient.patientId;
+                    labReportSummary.doctor = patient.investigation[i].report.publishedById;
+                    labReportSummary.status = patient.investigation[i].isUploaded;
+                    labReportSummary.date = patient.date;
+                    summary.push(labReportSummary);
+                });
+                labReportSummary.request = newLapRequest.map(x=>x.patientId).length;
+                laboratoryReport.summary = summary;
+                laboratoryReport.location =summaryByLoc;
+                laboratoryReport.bench = summaryByBench;
+                return laboratoryReport;
+            }
+        } catch (error) {
+            return jsend.error({
+                message: 'There was an errror while ',
+                code: 422,
+                data: {
+                    error
+                }
+            });
+        }
+
         return Promise.resolve([]);
     }
 
@@ -18,32 +170,7 @@ class Service {
 
 
     async create(data, params) {
-        const InvestigationService = this.app.service('investigations');
-        const LaboratoryRequestService = this.app.service('laboratory-requests');
-
-        let facilityId = data.facilityId;
-        try {
-            console.log('========Got here=========\n', facilityId);
-            const getLabRequest = await LaboratoryRequestService.find({
-                query: {
-                    facilityId: facilityId
-                }
-            });
-            console.log('========Got here1=========');
-            if (getLabRequest !== undefined) {
-                console.log('==============data=============\n', getLabRequest);
-            }
-        } catch (error) {
-            console.log('===error===\n', error);
-            return jsend.error({
-                message: 'There was an errror while ',
-                code: 422,
-                data: {
-                    error
-                }
-            });
-        }
-
+        
         if (Array.isArray(data)) {
             return Promise.all(data.map(current => this.create(current, params)));
         }
--- src/services/laboratory-requests/laboratory-requests.hooks.js
+++ src/services/laboratory-requests/laboratory-requests.hooks.js
@@ -1,58 +1,58 @@
 const {
-  authenticate
+    authenticate
 } = require('@feathersjs/authentication').hooks;
 const {
-  fastJoin
+    fastJoin
 } = require('feathers-hooks-common');
 const resolvers = {
-  joins: {
+    joins: {
 
-    patientDetails: () => async (request, context) => {
-      try {
-        const patient = await context.app.service('patients').get(request.patientId, {});
-        request.personDetails = patient.personDetails;
-      } catch (error) {}
+        patientDetails: () => async (request, context) => {
+            try {
+                const patient = await context.app.service('patients').get(request.patientId, {});
+                request.personDetails = patient.personDetails;
+            } catch (error) {}
 
-    },
-    employeeDetails: () => async (request, context) => {
-      try {
-        const employee = await context.app.service('employees').get(request.createdBy, {});
-        request.employeeDetails = employee.personDetails;
-      } catch (error) {}
+        },
+        employeeDetails: () => async (request, context) => {
+            try {
+                const employee = await context.app.service('employees').get(request.createdBy, {});
+                request.employeeDetails = employee.personDetails;
+            } catch (error) {}
 
+        }
     }
-  }
 };
 
 
 module.exports = {
-  before: {
-    all: [authenticate('jwt')],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  },
+    before: {
+        all: [authenticate('jwt')],
+        find: [],
+        get: [],
+        create: [],
+        update: [],
+        patch: [],
+        remove: []
+    },
 
-  after: {
-    all: [fastJoin(resolvers)],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  },
+    after: {
+        all: [fastJoin(resolvers)],
+        find: [],
+        get: [],
+        create: [],
+        update: [],
+        patch: [],
+        remove: []
+    },
 
-  error: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  }
+    error: {
+        all: [],
+        find: [],
+        get: [],
+        create: [],
+        update: [],
+        patch: [],
+        remove: []
+    }
 };
