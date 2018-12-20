71f59d6839af8b264f97e3657965313aa37af0a9
Sunday Orimoyegun <sunday.orimoyegun@apmis.ng>
2018-12-20 15:24:32 +0100

    updates

--- src/services/add-purchase-entries/add-purchase-entries.class.js
+++ src/services/add-purchase-entries/add-purchase-entries.class.js
@@ -25,63 +25,64 @@ class Service {
     const inventoriesService = this.app.service('inventories');
     const purchaseEntriesService = this.app.service('purchase-entries');
     const makePurchaseEntriesService = this.app.service('make-purchase-entries');
-    const purchaseEntry = {};
-    purchaseEntry.invoiceAmount = data.amount.toString();
-    purchaseEntry.deliveryDate = data.deliveryDate;
-    purchaseEntry.facilityId = data.facilityId;
-    purchaseEntry.invoiceNumber = data.invoiceNo;
-    purchaseEntry.orderId = data.orderId;
-    purchaseEntry.remark = data.desc;
-    purchaseEntry.storeId = data.store;
-    purchaseEntry.supplierId = data.supplier;
-    purchaseEntry.createdBy = data.createdBy;
+    const purchaseEntry = data;
+    console.log(data);
+    // purchaseEntry.invoiceAmount = data.amount.toString();
+    // purchaseEntry.deliveryDate = data.deliveryDate;
+    // purchaseEntry.facilityId = data.facilityId;
+    // purchaseEntry.invoiceNumber = data.invoiceNo;
+    // purchaseEntry.orderId = data.orderId;
+    // purchaseEntry.remark = data.desc;
+    // purchaseEntry.storeId = data.store;
+    // purchaseEntry.supplierId = data.supplier;
+    // purchaseEntry.createdBy = data.createdBy;
 
-    purchaseEntry.products = [];
+    // purchaseEntry.products = [];
+    // purchaseEntry = data;
 
     /* end*/
 
     let inventories = [];
     let existingInventories = [];
-    for (let index = 0; index < data.productForms.length; index++) {
-      let productObj = data.productForms[index];
-      if (productObj.productObject.config !== undefined) {
-        delete productObj.productObject.config;
-      }
-      // let productObject = {
-      //   id: productObj.id,
-      //   name: productObj.product,
-      //   code: productObj.code
+    for (let index = 0; index < data.products.length; index++) {
+      let productObj = data.products[index];
+      // if (productObj.productObject.config !== undefined) {
+      //   delete productObj.productObject.config;
       // }
-      let product = {};
-      product.batchNo = productObj.batchNo;
-      product.costPrice = productObj.costPrice;
-      product.expiryDate = productObj.expiryDate;
-      product.productId = productObj.id;
-      product.productObject = productObj.productObject;
-      product.quantity = productObj.qty;
-      product.qtyDetails = productObj.qtyDetails;
-      product.availableQuantity = productObj.qty;
-      purchaseEntry.products.push(product);
+
+      // let product = {};
+      // product.batchNo = productObj.batchNo;
+      // product.costPrice = productObj.costPrice;
+      // product.expiryDate = productObj.expiryDate;
+      // product.productId = productObj.productId;
+      // product.productObject = productObj.productObject;
+      // product.quantity = productObj.quantity;
+      // product.qtyDetails = productObj.qtyDetails;
+      // product.availableQuantity = productObj.quantity;
+      // purchaseEntry.products.push(product);
 
       const existingInventory = await inventoriesService.find({
         query: {
           facilityId: data.facilityId,
-          storeId: data.store,
-          productId: productObj.id
+          storeId: data.storeId,
+          productId: productObj.productId
         }
       });
-      const inventory = existingInventory.data[0];
-      inventory.totalQuantity = inventory.totalQuantity + productObj.qty;
-      inventory.availableQuantity = inventory.availableQuantity + productObj.qty;
-      const inventoryTransaction = {};
-      inventoryTransaction.batchNumber = productObj.batchNo;
-      inventoryTransaction.costPrice = productObj.costPrice;
-      inventoryTransaction.expiryDate = productObj.expiryDate;
-      inventoryTransaction.quantity = productObj.qty;
-      inventoryTransaction.availableQuantity = productObj.qty;
-      inventory.transactions.push(inventoryTransaction);
+      const inventory = existingInventory.data.length > 0 ? existingInventory.data[0] : undefined;
+      if (inventory !== undefined) {
+        inventory.totalQuantity = inventory.totalQuantity + productObj.quantity;
+        inventory.availableQuantity = inventory.availableQuantity + productObj.quantity;
+        const inventoryTransaction = {};
+        inventoryTransaction.batchNumber = productObj.batchNo;
+        inventoryTransaction.costPrice = productObj.costPrice;
+        inventoryTransaction.expiryDate = productObj.expiryDate;
+        inventoryTransaction.quantity = productObj.quantity;
+        inventoryTransaction.availableQuantity = productObj.quantity;
+        inventory.transactions.push(inventoryTransaction);
 
-      existingInventories.push(inventory);
+        existingInventories.push(inventory);
+      }
+
     };
 
     const data_ = {
