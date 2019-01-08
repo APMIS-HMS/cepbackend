7cb157a48f1097d925d26f7d374a46724ebce2a5
Sunday Orimoyegun <sunday.orimoyegun@apmis.ng>
2019-01-08 23:36:38 +0100

    updates

--- src/services/inventory-initialisers/inventory-initialisers.class.js
+++ src/services/inventory-initialisers/inventory-initialisers.class.js
@@ -2,211 +2,229 @@
 const jsend = require('jsend');
 const requestPromise = require('request-promise');
 class Service {
-	constructor(options) {
-		this.options = options || {};
-	}
+  constructor(options) {
+    this.options = options || {};
+  }
 
-	setup(app) {
-		this.app = app;
-	}
+  setup(app) {
+    this.app = app;
+  }
 
-	find(params) {
-		return Promise.resolve([]);
-	}
+  find(params) {
+    return Promise.resolve([]);
+  }
 
-	get(id, params) {
-		return Promise.resolve({
-			id,
-			text: `A new message with ID: ${id}!`
-		});
-	}
+  get(id, params) {
+    return Promise.resolve({
+      id,
+      text: `A new message with ID: ${id}!`
+    });
+  }
 
-	async create(data, params) {
-		const inventoriesService = this.app.service('inventories');
-		const productsService = this.app.service('products');
-		const orgService = this.app.service('organisation-services');
-		const facilityPriceService = this.app.service('facility-prices');
-		if (params.query.isArray) {
-			const facilityId = data[0].product.facilityId;
-			const storeId = data[0].storeId;
-			const productIds = data.map((product) => {
-				return product.product.productObject.id;
-			});
-			let inventory = await inventoriesService.find({
-				query: {
-					facilityId: facilityId,
-					productId: {
-						$in: productIds
-					},
-					storeId: storeId
-				}
-			});
+  async create(data, params) {
+    const inventoriesService = this.app.service('inventories');
+    const productsService = this.app.service('products');
+    const orgService = this.app.service('organisation-services');
+    const facilityPriceService = this.app.service('facility-prices');
+    if (params.query.isArray) {
+      const facilityId = data[0].product.facilityId;
+      const storeId = data[0].storeId;
+      const productIds = data.map((product) => {
+        return product.product.productObject.id;
+      });
+      let inventory = await inventoriesService.find({
+        query: {
+          facilityId: facilityId,
+          productId: {
+            $in: productIds
+          },
+          storeId: storeId
+        }
+      });
 
-			let resultList = [];
-			try {
-				for (const product of data) {
-					let service = {};
-					let index = null;
-					let orgServiceValue = {};
-					service.name = product.product.productObject.name;
-					let awaitOrganService = await orgService.get(product.facilityServiceId);
-					try {
-						awaitOrganService.categories.forEach((item, i) => {
-							if (item._id.toString() === product.categoryId.toString()) {
-								item.services.push(service);
-								index = i;
-							}
-						});
-					} catch (error) {}
+      let resultList = [];
+      try {
+        for (const product of data) {
+          let service = {};
+          let index = null;
+          let orgServiceValue = {};
+          service.name = product.product.productObject.name;
+          let awaitOrganService = await orgService.get(product.facilityServiceId);
+          try {
+            awaitOrganService.categories.forEach((item, i) => {
+              if (item._id.toString() === product.categoryId.toString()) {
+                item.services.push(service);
+                index = i;
+              }
+            });
+          } catch (error) {}
 
-					const payResult = await orgService.patch(awaitOrganService._id, awaitOrganService);
-					payResult.categories.forEach((itemi, i) => {
-						if (itemi._id.toString() === product.categoryId.toString()) {
-							itemi.services.forEach((items, s) => {
-								if (items.name === service.name) {
-									orgServiceValue.serviceId = items._id;
-								}
-							});
-						}
-					});
+          const payResult = await orgService.patch(awaitOrganService._id, awaitOrganService);
+          payResult.categories.forEach((itemi, i) => {
+            if (itemi._id.toString() === product.categoryId.toString()) {
+              itemi.services.forEach((items, s) => {
+                if (items.name === service.name) {
+                  orgServiceValue.serviceId = items._id;
+                }
+              });
+            }
+          });
 
-					let batches = product;
-					let inventoryModel = {};
-					inventoryModel.facilityId = batches.product.facilityId;
-					inventoryModel.storeId = batches.storeId;
-					inventoryModel.serviceId = orgServiceValue.serviceId;
-					inventoryModel.categoryId = product.categoryId;
-					inventoryModel.facilityServiceId = product.facilityServiceId;
-					inventoryModel.productId = batches.product.productObject.id;
-					inventoryModel.productObject = batches.product.productObject;
-					inventoryModel.transactions = [];
-					inventoryModel.totalQuantity = 0;
-					inventoryModel.availableQuantity = 0;
-					inventoryModel.costPrice = product.costPrice;
+          let batches = product;
+          let inventoryModel = {};
+          inventoryModel.facilityId = batches.product.facilityId;
+          inventoryModel.storeId = batches.storeId;
+          inventoryModel.serviceId = orgServiceValue.serviceId;
+          inventoryModel.categoryId = product.categoryId;
+          inventoryModel.facilityServiceId = product.facilityServiceId;
+          inventoryModel.productId = batches.product.productObject.id;
+          inventoryModel.productObject = batches.product.productObject;
+          inventoryModel.transactions = [];
+          inventoryModel.totalQuantity = 0;
+          inventoryModel.availableQuantity = 0;
+          inventoryModel.costPrice = product.costPrice;
 
-					const url = process.env.NODE_ENV
-						? process.env.NODE_ENV
-						: 'http://localhost:3030' + '/get-scdc-by-sbd-code/' + inventoryModel.productObject.code;
+          const url = process.env.NODE_ENV ?
+            process.env.NODE_ENV :
+            'http://localhost:3030' + '/get-scdc-by-sbd-code/' + inventoryModel.productObject.code;
 
-					const options = {
-						method: 'GET',
-						uri: url
-					};
+          const url2 = process.env.NODE_ENV ?
+            process.env.NODE_ENV :
+            'http://localhost:3030' + '/get-scdc-by-sbd-code/' + inventoryModel.productObject.code + '?fromSCD=true';
 
-					try {
-						const makeRequest = await requestPromise(options);
-						const parsed = JSON.parse(makeRequest);
-						inventoryModel.productObject.scdc = parsed.CODE;
-					} catch (e) {}
+          const options = {
+            method: 'GET',
+            uri: url
+          };
 
-					let len = batches.batchItems.length - 1;
-					for (let index = len; index >= 0; index--) {
-						if (index >= 0) {
-							inventoryModel.totalQuantity += batches.batchItems[index].quantity;
-							inventoryModel.availableQuantity += batches.batchItems[index].quantity;
-							inventoryModel.transactions.push(batches.batchItems[index]);
-						}
-					}
-					inventoryModel.margin = product.margin;
-					let inventory = await inventoriesService.create(inventoryModel);
-					let res = {
-						inventory: inventory
-					};
+          const options2 = {
+            method: 'GET',
+            uri: url2
+          };
 
-					let price = {
-						facilityServiceId: inventory.facilityServiceId,
-						categoryId: inventory.categoryId,
-						serviceId: inventory.serviceId,
-						modifieres: [],
-						price: product.sellingPrice,
-						facilityId: facilityId
-					};
+          try {
+            const [result1, result2] = await Promise.all([requestPromise(options), requestPromise(options2)]);
+            let parsed;
+            if (JSON.parse(result1).length > 0) {
+              parsed = JSON.parse(result1);
+            } else if (JSON.parse(result2).length > 0) {
+              parsed = JSON.parse(result2);
+            } else {
+              parsed = [];
+            }
+            // const parsed = JSON.parse(makeRequest);
+            inventoryModel.productObject.ingredientCodes = parsed.map(ing => ing.CODE);
+            inventoryModel.productObject.ingredientNames = parsed.map(ing => ing.STR);
+          } catch (e) {}
 
-					const createdPrice = await facilityPriceService.create(price);
+          let len = batches.batchItems.length - 1;
+          for (let index = len; index >= 0; index--) {
+            if (index >= 0) {
+              inventoryModel.totalQuantity += batches.batchItems[index].quantity;
+              inventoryModel.availableQuantity += batches.batchItems[index].quantity;
+              inventoryModel.transactions.push(batches.batchItems[index]);
+            }
+          }
+          inventoryModel.margin = product.margin;
+          let inventory = await inventoriesService.create(inventoryModel);
+          let res = {
+            inventory: inventory
+          };
 
-					resultList.push(res);
-				}
-				return jsend.success(resultList);
-			} catch (error) {}
-		} else {
-			let inventory = await inventoriesService.find({
-				query: {
-					facilityId: data.product.facilityId,
-					productId: data.product.productObject.id,
-					storeId: data.storeId
-				}
-			});
-			if (inventory.data.length > 0) {
-				return {};
-			} else {
-				let service = {};
-				let index = null;
-				let orgServiceValue = {};
-				service.name = data.product.productObject.name;
-				let awaitOrganService = await orgService.get(data.facilityServiceId);
-				awaitOrganService.categories.forEach((item, i) => {
-					if (item._id.toString() === data.categoryId.toString()) {
-						item.services.push(service);
-						index = i;
-					}
-				});
-				const payResult = await orgService.patch(awaitOrganService._id, awaitOrganService);
-				payResult.categories.forEach((itemi, i) => {
-					if (itemi._id.toString() === data.categoryId.toString()) {
-						itemi.services.forEach((items, s) => {
-							if (items.name === service.name) {
-								orgServiceValue.serviceId = items._id;
-							}
-						});
-					}
-				});
-				let batches = data;
-				let inventoryModel = {};
-				inventoryModel.facilityId = batches.product.facilityId;
-				inventoryModel.storeId = batches.storeId;
-				inventoryModel.serviceId = orgServiceValue.serviceId;
-				inventoryModel.categoryId = data.categoryId;
-				inventoryModel.facilityServiceId = data.facilityServiceId;
-				inventoryModel.productId = batches.product.productObject.id;
-				inventoryModel.productObject = batches.product.productObject;
-				inventoryModel.transactions = [];
-				inventoryModel.totalQuantity = 0;
-				inventoryModel.availableQuantity = 0;
+          let price = {
+            facilityServiceId: inventory.facilityServiceId,
+            categoryId: inventory.categoryId,
+            serviceId: inventory.serviceId,
+            modifieres: [],
+            price: product.sellingPrice,
+            facilityId: facilityId
+          };
 
-				let len = batches.batchItems.length - 1;
-				for (let index = len; index >= 0; index--) {
-					if (index >= 0) {
-						inventoryModel.totalQuantity += batches.batchItems[index].quantity;
-						inventoryModel.availableQuantity += batches.batchItems[index].quantity;
-						inventoryModel.transactions.push(batches.batchItems[index]);
-					}
-				}
-				let inventory = await inventoriesService.create(inventoryModel);
-				let res = {
-					inventory: inventory
-				};
-				return res;
-			}
-		}
-	}
+          const createdPrice = await facilityPriceService.create(price);
 
-	update(id, data, params) {
-		return Promise.resolve(data);
-	}
+          resultList.push(res);
+        }
+        return jsend.success(resultList);
+      } catch (error) {}
+    } else {
+      let inventory = await inventoriesService.find({
+        query: {
+          facilityId: data.product.facilityId,
+          productId: data.product.productObject.id,
+          storeId: data.storeId
+        }
+      });
+      if (inventory.data.length > 0) {
+        return {};
+      } else {
+        let service = {};
+        let index = null;
+        let orgServiceValue = {};
+        service.name = data.product.productObject.name;
+        let awaitOrganService = await orgService.get(data.facilityServiceId);
+        awaitOrganService.categories.forEach((item, i) => {
+          if (item._id.toString() === data.categoryId.toString()) {
+            item.services.push(service);
+            index = i;
+          }
+        });
+        const payResult = await orgService.patch(awaitOrganService._id, awaitOrganService);
+        payResult.categories.forEach((itemi, i) => {
+          if (itemi._id.toString() === data.categoryId.toString()) {
+            itemi.services.forEach((items, s) => {
+              if (items.name === service.name) {
+                orgServiceValue.serviceId = items._id;
+              }
+            });
+          }
+        });
+        let batches = data;
+        let inventoryModel = {};
+        inventoryModel.facilityId = batches.product.facilityId;
+        inventoryModel.storeId = batches.storeId;
+        inventoryModel.serviceId = orgServiceValue.serviceId;
+        inventoryModel.categoryId = data.categoryId;
+        inventoryModel.facilityServiceId = data.facilityServiceId;
+        inventoryModel.productId = batches.product.productObject.id;
+        inventoryModel.productObject = batches.product.productObject;
+        inventoryModel.transactions = [];
+        inventoryModel.totalQuantity = 0;
+        inventoryModel.availableQuantity = 0;
 
-	patch(id, data, params) {
-		return Promise.resolve(data);
-	}
+        let len = batches.batchItems.length - 1;
+        for (let index = len; index >= 0; index--) {
+          if (index >= 0) {
+            inventoryModel.totalQuantity += batches.batchItems[index].quantity;
+            inventoryModel.availableQuantity += batches.batchItems[index].quantity;
+            inventoryModel.transactions.push(batches.batchItems[index]);
+          }
+        }
+        let inventory = await inventoriesService.create(inventoryModel);
+        let res = {
+          inventory: inventory
+        };
+        return res;
+      }
+    }
+  }
 
-	remove(id, params) {
-		return Promise.resolve({
-			id
-		});
-	}
+  update(id, data, params) {
+    return Promise.resolve(data);
+  }
+
+  patch(id, data, params) {
+    return Promise.resolve(data);
+  }
+
+  remove(id, params) {
+    return Promise.resolve({
+      id
+    });
+  }
 }
 
-module.exports = function(options) {
-	return new Service(options);
+module.exports = function (options) {
+  return new Service(options);
 };
 
 module.exports.Service = Service;
--- src/services/list-of-inventories/list-of-inventories.class.js
+++ src/services/list-of-inventories/list-of-inventories.class.js
@@ -1,142 +1,270 @@
 /* eslint-disable no-unused-vars */
 var difference_in_calendar_days = require('date-fns/difference_in_calendar_days');
+const request = require('request');
+const requestPromise = require('request-promise');
+const jsend = require('jsend');
 class Service {
-    constructor(options) {
-        this.options = options || {};
+  constructor(options) {
+    this.options = options || {};
+  }
+
+  setup(app) {
+    this.app = app;
+  }
+
+  async _find(params) {
+    const inventoriesService = this.app.service('inventories');
+    const fpService = this.app.service('formulary-products');
+    let _inventories = {};
+    _inventories.data = [];
+
+    console.log(params.query.name);
+    let productIds = await fpService.find({
+      query: {
+        name: params.query.name
+      }
+    });
+    console.log(productIds);
+    for (let index = 0; index < productIds.data.length; index++) {
+      const inventories = await inventoriesService.find({
+        query: {
+          facilityId: params.query.facilityId,
+          storeId: params.query.storeId,
+          productId: productIds.data[index].id
+        }
+      });
+      if (inventories.data[0] !== undefined) {
+        _inventories.data.push(inventories.data[0]);
+      }
     }
+    // let inventoriesDefined = inventories.data.filter(x => x.productObject !== undefined);
 
-    setup(app) {
-        this.app = app;
-    }
+    // const filter = inventoriesDefined.filter(x => x.productObject.name.includes(params.query.name));
+    // console.log(filter)
+    // _inventories.data = inventories.data; //filter;
+    if (_inventories.data.length > 0) {
+      for (let index2 = _inventories.data.length - 1; index2 >= 0; index2--) {
+        if (_inventories.data[index2].transactions.length > 0) {
+          let len3 = _inventories.data[index2].transactions.length - 1;
+          for (let index3 = len3; index3 >= 0; index3--) {
+            if (_inventories.data[index2].transactions[index3].expiryDate != undefined) {
 
-    async find(params) {
-        const inventoriesService = this.app.service('inventories');
-        const fpService = this.app.service('formulary-products');
-        let _inventories = {};
-        _inventories.data = [];
-
-        console.log(params.query.name);
-        let productIds = await fpService.find({
-            query: {
-                name: params.query.name
+              _inventories.data[index2].transactions[index3].expiration = expiration(_inventories.data[index2].transactions[index3].expiryDate, new Date());
             }
+            if (_inventories.data[index2].transactions[index3].availableQuantity <= 0) {
+              _inventories.data[index2].transactions.splice(index3, 1);
+            }
+          }
+        }
+      }
+      return _inventories;
+    } else {
+      let result = {
+        data: []
+      };
+      return result;
+    }
+  }
+
+  async find(params) {
+    const inventoriesService = this.app.service('inventories');
+    const fpService = this.app.service('formulary-products');
+    const url = process.env.NODE_ENV ? process.env.NODE_ENV : 'http://localhost:3030' + '/get-scdc-by-sbd-code/' + params.query.code + '?fromSCD=true';
+
+    const options = {
+      method: 'GET',
+      uri: url
+    };
+
+    try {
+      const makeRequest = await requestPromise(options);
+      const parsed = JSON.parse(makeRequest);
+
+
+
+
+
+
+
+
+
+
+      let regexList = [];
+      parsed.map(par => par.CODE).forEach((value) => {
+        regexList.push({
+          'productObject.ingredientCodes': {
+            $regex: value,
+            $options: 'gmi',
+            $size: parsed.length
+          },
         });
-        console.log(productIds);
-        for (let index = 0; index < productIds.data.length; index++) {
-            const inventories = await inventoriesService.find({
-                query: {
-                    facilityId: params.query.facilityId,
-                    storeId: params.query.storeId,
-                    productId: productIds.data[index].id
-                }
-            });
-            if(inventories.data[0]!== undefined){
-                _inventories.data.push(inventories.data[0]);
+      });
+      // db.coll.find({
+      //   "hobbies": {
+      //     "$size": 2,
+      //     "$all": ["2", "1"]
+      //   }
+      // });
+
+      console.log(...regexList);
+      const awaitConsoService = await inventoriesService.find({
+        query: {
+          $and: [...regexList]
+        }
+      });
+
+
+      console.log(awaitConsoService.data.map(d => d.productObject));
+
+
+
+
+
+
+
+
+      const inventoriesAwait = await inventoriesService.find({
+        query: {
+          'productObject.ingredientCodes': {
+            $in: parsed.map(par => par.CODE)
+          }
+        }
+      });
+      // console.log(inventoriesAwait);
+      return {
+        data: inventoriesAwait.data
+      };
+    } catch (error) {
+      console.log(error);
+    }
+
+
+
+    let _inventories = {};
+    _inventories.data = [];
+
+    console.log(params.query.name);
+    let productIds = await fpService.find({
+      query: {
+        name: params.query.name
+      }
+    });
+    console.log(productIds);
+    for (let index = 0; index < productIds.data.length; index++) {
+      const inventories = await inventoriesService.find({
+        query: {
+          facilityId: params.query.facilityId,
+          storeId: params.query.storeId,
+          productId: productIds.data[index].id
+        }
+      });
+      if (inventories.data[0] !== undefined) {
+        _inventories.data.push(inventories.data[0]);
+      }
+    }
+    // let inventoriesDefined = inventories.data.filter(x => x.productObject !== undefined);
+
+    // const filter = inventoriesDefined.filter(x => x.productObject.name.includes(params.query.name));
+    // console.log(filter)
+    // _inventories.data = inventories.data; //filter;
+    if (_inventories.data.length > 0) {
+      for (let index2 = _inventories.data.length - 1; index2 >= 0; index2--) {
+        if (_inventories.data[index2].transactions.length > 0) {
+          let len3 = _inventories.data[index2].transactions.length - 1;
+          for (let index3 = len3; index3 >= 0; index3--) {
+            if (_inventories.data[index2].transactions[index3].expiryDate != undefined) {
+
+              _inventories.data[index2].transactions[index3].expiration = expiration(_inventories.data[index2].transactions[index3].expiryDate, new Date());
             }
-        }
-        // let inventoriesDefined = inventories.data.filter(x => x.productObject !== undefined);
-
-        // const filter = inventoriesDefined.filter(x => x.productObject.name.includes(params.query.name));
-        // console.log(filter)
-        // _inventories.data = inventories.data; //filter;
-        if (_inventories.data.length > 0) {
-            for (let index2 = _inventories.data.length - 1; index2 >= 0; index2--) {
-                if (_inventories.data[index2].transactions.length > 0) {
-                    let len3 = _inventories.data[index2].transactions.length - 1;
-                    for (let index3 = len3; index3 >= 0; index3--) {
-                        if (_inventories.data[index2].transactions[index3].expiryDate != undefined) {
-
-                            _inventories.data[index2].transactions[index3].expiration = expiration(_inventories.data[index2].transactions[index3].expiryDate, new Date());
-                        }
-                        if (_inventories.data[index2].transactions[index3].availableQuantity <= 0) {
-                            _inventories.data[index2].transactions.splice(index3, 1);
-                        }
-                    }
-                }
+            if (_inventories.data[index2].transactions[index3].availableQuantity <= 0) {
+              _inventories.data[index2].transactions.splice(index3, 1);
             }
-            return _inventories;
-        } else {
-            let result = {
-                data: []
-            };
-            return result;
+          }
         }
+      }
+      return _inventories;
+    } else {
+      let result = {
+        data: []
+      };
+      return result;
+    }
+  }
+
+  async get(id, params) {
+
+  }
+
+  create(data, params) {
+    if (Array.isArray(data)) {
+      return Promise.all(data.map(current => this.create(current)));
     }
 
-    async get(id, params) {
+    return Promise.resolve(data);
+  }
 
-    }
+  update(id, data, params) {
+    return Promise.resolve(data);
+  }
 
-    create(data, params) {
-        if (Array.isArray(data)) {
-            return Promise.all(data.map(current => this.create(current)));
-        }
+  patch(id, data, params) {
+    return Promise.resolve(data);
+  }
 
-        return Promise.resolve(data);
-    }
-
-    update(id, data, params) {
-        return Promise.resolve(data);
-    }
-
-    patch(id, data, params) {
-        return Promise.resolve(data);
-    }
-
-    remove(id, params) {
-        return Promise.resolve({
-            id
-        });
-    }
+  remove(id, params) {
+    return Promise.resolve({
+      id
+    });
+  }
 }
 
 function expiration(date1, date2) {
-    let days = difference_in_calendar_days(date1, date2);
-    let dateRemains = '';
-    let rem = days % 365;
-    if (rem > 0) {
-        let rationalNo = days - rem;
-        let year = rationalNo / 365;
-        if (year >= 2) {
-            dateRemains = year + ' Years ';
-        } else if (year == 1) {
-            dateRemains = year + ' Year ';
-        }
-        let mRem = rem % 31;
-        if (mRem > 0) {
-            let nRational = rem - mRem;
-            let m = nRational / 31;
-            if (m >= 2) {
-                dateRemains += m + ' Months ';
-            } else if (m == 1) {
-                dateRemains += m + ' Month ';
-            }
-            if (mRem >= 2) {
-                dateRemains += mRem + ' Days ';
-            } else if (mRem == 1) {
-                dateRemains += mRem + ' Day ';
-            }
-        } else {
-            mRem = rem / 31;
-            if (mRem >= 2) {
-                dateRemains += mRem + ' Months ';
-            } else if (mRem == 1) {
-                dateRemains += mRem + ' Month ';
-            }
-        }
-    } else {
-        rem = days / 365;
-        if (rem >= 2) {
-            dateRemains = rem + ' Years ';
-        } else if (rem == 1) {
-            dateRemains = rem + ' Year ';
-        }
+  let days = difference_in_calendar_days(date1, date2);
+  let dateRemains = '';
+  let rem = days % 365;
+  if (rem > 0) {
+    let rationalNo = days - rem;
+    let year = rationalNo / 365;
+    if (year >= 2) {
+      dateRemains = year + ' Years ';
+    } else if (year == 1) {
+      dateRemains = year + ' Year ';
     }
-    return dateRemains;
+    let mRem = rem % 31;
+    if (mRem > 0) {
+      let nRational = rem - mRem;
+      let m = nRational / 31;
+      if (m >= 2) {
+        dateRemains += m + ' Months ';
+      } else if (m == 1) {
+        dateRemains += m + ' Month ';
+      }
+      if (mRem >= 2) {
+        dateRemains += mRem + ' Days ';
+      } else if (mRem == 1) {
+        dateRemains += mRem + ' Day ';
+      }
+    } else {
+      mRem = rem / 31;
+      if (mRem >= 2) {
+        dateRemains += mRem + ' Months ';
+      } else if (mRem == 1) {
+        dateRemains += mRem + ' Month ';
+      }
+    }
+  } else {
+    rem = days / 365;
+    if (rem >= 2) {
+      dateRemains = rem + ' Years ';
+    } else if (rem == 1) {
+      dateRemains = rem + ' Year ';
+    }
+  }
+  return dateRemains;
 }
 
 module.exports = function (options) {
-    return new Service(options);
+  return new Service(options);
 };
 
 module.exports.Service = Service;
